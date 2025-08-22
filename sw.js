6e407f8a7bcdc70243282c2835bb74c681e9f4eb
AR1STIKA <puppyboo522@gmail.com>
2025-08-23 09:22:56 +1000

    seo improve+new post

---
+++ sw.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = 'dreamgarden-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/features.html',
+  '/pricing.html',
+  '/posts.html',
+  '/launch-announcement.html',
+  '/spring-planting-guide.html',
+  '/september-gardening-guide.html',
+  '/privacy.html',
+  '/404.html',
+  '/favicon.svg',
+  '/manifest.json',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
